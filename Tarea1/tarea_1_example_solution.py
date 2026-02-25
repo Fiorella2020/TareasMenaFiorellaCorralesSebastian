1b867ec160838daf3144a423521a0673ac6b58ea
Sebastián Corrales Hernández <secoher26@gmail.com>
2026-02-25 15:43:43 -0600

    Código con ambos métodos que pasó el pytest

--- Tarea1/tarea_1_example_solution.py
+++ Tarea1/tarea_1_example_solution.py
@@ -1,13 +1,10 @@
+import pytest
 def filtrar_vocales(cadena, bandera):
     # a) cadena debe ser string
     if not isinstance(cadena, str):
         return -100, None
 
-    # b) cadena debe contener solo letras del abecedario
-    if not cadena.isalpha():
-        return -200, None
-    
-    # c) cadena no debe estar vacía
+    # c) cadena no debe estar vacía  (IMPORTANTE: antes de isalpha)
     if cadena == "":
         return -300, None
 
@@ -16,19 +13,20 @@ def filtrar_vocales(cadena, bandera):
         return -400, None
 
     # e) bandera debe ser booleano real (True/False)
-    # OJO: bool es subclase de int, por eso usamos type(...) is bool
     if type(bandera) is not bool:
         return -500, None
 
-    # f y g) filtrar según bandera
+    # b) cadena debe contener solo letras
+    if not cadena.isalpha():
+        return -200, None
+
     vocales = "aeiouAEIOU"
 
-    if bandera is True:
-        filtrado = "".join(car for car in cadena if car in vocales)
+    if bandera:
+        filtrado = "".join(c for c in cadena if c in vocales)
     else:
-        filtrado = "".join(car for car in cadena if car not in vocales)
+        filtrado = "".join(c for c in cadena if c not in vocales)
 
-    # h + i) código de éxito + string filtrado
     return 0, filtrado
 
 def encontrar_extremos(lista):
